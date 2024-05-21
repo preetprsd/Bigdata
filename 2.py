1828e52842c6a10fec66740e256361ef1302d6c2
preetprsd <165990722+preetprsd@users.noreply.github.com>
2024-05-21 10:38:22 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,24 +1,22 @@
 from mrjob.job import MRJob
 
-class OddEven(MRJob):
+class Odd(MRJob):
     def mapper(self, _, line):
         row = line.split()
-        for i in row:
-            try:
-                num = int(i)
-                if num % 2:
-                    yield "odd", (1, num)
-                else:
-                    yield "even", (1, num)
-            except ValueError:
-                pass
+        for num in row:
+            num = int(num)
+            if num % 2 == 0:
+                yield "even", num
+            else:
+                yield "odd", num
 
     def reducer(self, key, values):
-        values_list = list(values)
-        cnt = sum(i[0] for i in values_list)
-        total = sum(i[1] for i in values_list)
-        yield "Sum of " + key + " numbers is :",str(total)
-        yield "Count of " + key + " numbers is: ", str(cnt)
+        tot = 0
+        cnt = 0
+        for v in values:
+            tot += v
+            cnt += 1
+        yield "the sum is: " + str(tot) + " and the freq is:", cnt
 
 if __name__ == '__main__':
-    OddEven.run()
+    Odd.run()
